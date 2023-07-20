883718477cad38822fde1191b3a9928752987781
Vicente Marçal <vicente.marcal@gmail.com>
2023-07-19 22:14:06 -0300

    feat(issue21): Ajustadas as configurações do projeto com Dynaconf. Close #21

---
+++ todo/config.py
@@ -0,0 +1,14 @@
+import os
+
+from dynaconf import Dynaconf
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+settings = Dynaconf(
+    envvar_prefix='todo',
+    preload=[os.path.join(HERE), 'default.toml'],
+    settings_files=['settings.toml', '.secrets.toml'],
+    environments=['development', 'production', 'testing'],
+    env_switcher='todo_env',
+    load_dotenv=False
+)
