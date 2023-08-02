eb50358000298c605186d488743e15a3170cd371
Vicente Marçal <vicente.marcal@gmail.com>
2023-08-02 20:45:56 -0300

    fix(issue30): Corrigido o problema do default.toml para usuo correto do Dynaconf.
    Close #30

--- todo/config.py
+++ todo/config.py
@@ -6,7 +6,7 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 
 settings = Dynaconf(
     envvar_prefix='todo',
-    preload=[os.path.join(HERE), 'default.toml'],
+    preload=[os.path.join(HERE, 'default.toml')],
     settings_files=['settings.toml', '.secrets.toml'],
     environments=['development', 'production', 'testing'],
     env_switcher='todo_env',
