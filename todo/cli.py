9d320b8b9744b9a1e63758123af601c6b9adced5
Vicente Marçal <vicente.marcal@gmail.com>
2023-08-02 22:04:50 -0300

    feat(issue26): Iniciad a implementação da ferramenta de CLI com shell e user-list. (#35)

---
+++ todo/cli.py
@@ -0,0 +1,49 @@
+import typer
+from rich.console import Console
+from rich.table import Table
+from sqlmodel import Session, select
+
+from todo.config import settings
+from todo.db import engine
+from todo.models import User
+
+main = typer.Typer(name='Todo CLI', add_completion=False)
+
+
+@main.command()
+def shell():
+    """Opens interactive shell"""
+    _vars = {
+        'settings': settings,
+        'engine': engine,
+        'select': select,
+        'session': Session(engine),
+        'User': User
+    }
+    typer.echo(f'Auto imports: {list(_vars.keys())}')
+    try:
+        from IPython import start_ipython
+
+        start_ipython(
+            argv=['--ipython-dir=/tmp', '--no-banner'], user_ns=_vars
+        )
+    except ImportError:
+        import code
+
+        code.InteractiveConsole(_vars).interact()
+
+
+@main.command()
+def user_list():
+    """Lists all users"""
+    table = Table(title='Todo Users List')
+    fields = ['name', 'user_name', 'active', 'created_at']
+    for header in fields:
+        table.add_column(header, style='red')
+
+    with Session(engine) as session:
+        users = session.exec(select(User))
+        for user in users:
+            table.add_row(*[getattr(user, field) for field in fields])
+
+    Console().print(table)
