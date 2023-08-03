9de2d9ed4ceb93a32c5bfa61e0ea2c859000822f
Vicente Marçal <vicente.marcal@gmail.com>
2023-08-02 21:07:03 -0300

    fix(issue31): Adicionadi o campo de user_name na tabela de User e alterado forma de registro do updated_at. (#34)

--- todo/models/user.py
+++ todo/models/user.py
@@ -9,6 +9,11 @@ class User(SQLModel, table=True):
     name: str = Field(nullable=True)
     email: str = Field(nullable=True, unique=True)
     password: str = Field(nullable=False)
+    user_name: str = Field(nullable=False, unique=True)
     active: Optional[bool] = Field(default=True)
     created_at: Optional[datetime] = Field(default_factory=datetime.utcnow)
-    updated_at: Optional[datetime] = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(
+        default_factory=datetime.utcnow,
+        nullable=False,
+        sa_column_kwargs={'onupdate': datetime.utcnow}
+    )
