9c426867b775c60644899ff404f6fe1b13803a65
Vicente Marçal <vicente.marcal@gmail.com>
2023-08-30 21:24:14 -0300

    feat(issue51): Preparado projeto para as rotas de autenticação.
    Close #51

--- todo/auth.py
+++ todo/auth.py
@@ -1,59 +1,145 @@
 """Token based auth"""
 from datetime import datetime, timedelta
+from typing import Callable, Optional, Union
 from functools import partial
-from fastapi import HTTPException, status
 
+from fastapi import Depends, HTTPException, Request, status
+from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from pydantic import BaseModel
+from sqlmodel import Session, select
+
 from todo.config import settings
-from todo.models import get_user, User
+from todo.db import engine
+from todo.models.user import User
+from todo.security import verify_password
 
-SECRET_KEY = settings.security.SECRET_KEY
-ALGORITHM = settings.security.ALGORITHM
+SECRET_KEY = settings.security.secret_key  # pyright: ignore
+ALGORITHM = settings.security.algorithm  # pyright: ignore
 
 
-# Models 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+# Models
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+
+class RefreshToken(BaseModel):
+    refresh_token: str
+
+
 class TokenData(BaseModel):
-    user_name: str | None
+    username: Optional[str] = None
 
 
 # Functions
-def create_access_token(data: dict, expires_delta: timedelta = None, scope: str = 'access_token') -> str:
-    """Creates a JWT Token from user data"""
+
+
+def create_access_token(
+    data: dict,
+    expires_delta: Optional[timedelta] = None,
+    scope: str = "access_token",
+) -> str:
+    """Creates a JWT Token from user data
+
+    scope: access_token or refresh_token
+    """
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=15)
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
-    to_encode.update({'exp': expire, 'scope': scope})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire, "scope": scope})
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,  # pyright: ignore
+        algorithm=ALGORITHM,  # pyright: ignore
+    )
     return encoded_jwt
 
 
-create_refresh_token = partial(create_access_token, scope='refresh_token')
+create_refresh_token = partial(create_access_token, scope="refresh_token")
 
 
-def get_current_user(token: str):
-    """Get current user authenticated"""
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail='Could not validate credentials.',
-        headers={'WWW-Authenticate': 'Bearer'}
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_name = payload.get('sub')
-        if user_name is None:
-            raise credentials_exception
-        token_data = TokenData(user_name=user_name)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(user_name=token_data.user_name)
-    if user is None:
-        raise credentials_exception
+def authenticate_user(
+    get_user: Callable, username: str, password: str
+) -> Union[User, bool]:
+    """Authenticate the user"""
+    user = get_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
     return user
 
 
-def validate_token(token: str) -> User:
+def get_user(username) -> Optional[User]:
+    """Get user from database"""
+    query = select(User).where(User.username == username)
+    with Session(engine) as session:
+        return session.exec(query).first()
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    request: Request = None,  # pyright: ignore
+    fresh=False
+) -> User:
+    """Get current user authenticated"""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    if request:
+        if authorization := request.headers.get("authorization"):
+            try:
+                token = authorization.split(" ")[1]
+            except IndexError:
+                raise credentials_exception
+
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,  # pyright: ignore
+            algorithms=[ALGORITHM]  # pyright: ignore
+        )
+        username: str = payload.get("sub")  # pyright: ignore
+
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    if fresh and (not payload["fresh"] and not user.superuser):
+        raise credentials_exception
+
+    return user
+
+
+# FastAPI dependencies
+
+async def get_current_active_user(
+    current_user: User = Depends(get_current_user),
+) -> User:
+    """Wraps the sync get_active_user for sync calls"""
+    return current_user
+
+
+AuthenticatedUser = Depends(get_current_active_user)
+
+
+async def validate_token(token: str = Depends(oauth2_scheme)) -> User:
     """Validates user token"""
     user = get_current_user(token=token)
     return user
