8ad2aa6a4870ed925a1c91fa71e0e9a3edfcfc27
Vicente Marçal <vicente.marcal@gmail.com>
2023-10-04 21:24:50 -0300

    feat(issue56): Criadas rotas para token e refresh token. (#62)
    Close #56

--- todo/routes/__init__.py
+++ todo/routes/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from .user import router as router_user
+from .auth import router as router_auth
 
 main_router = APIRouter()
 
 main_router.include_router(router_user, prefix='/user', tags=['user'])
+main_router.include_router(router_auth, tags=['authentication'])
---
+++ todo/routes/auth.py
@@ -0,0 +1,57 @@
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from todo.auth import RefreshToken, Token, authenticate_user, create_access_token, create_refresh_token, validate_token
+from todo.config import settings
+from todo.models import User, get_user
+
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.security.access_token_expire_minutes
+REFRESH_TOKEN_EXPIRE_MINUTES = settings.security.refresh_token_expire_minutes
+
+router = APIRouter()
+
+
+@router.post('/token', response_model=Token)
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends()
+):
+    user = authenticate_user(get_user, form_data.username, form_data.password)
+    if not user or not isinstance(user, User):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Incorrect username or password.',
+            headers={'WWW-Authenticate': 'Bearer'}
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={'sub': user.user_name, 'fresh': True}, expires_delta=access_token_expires
+    )
+    refresh_token_expires = timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+    refresh_token = create_refresh_token(
+        data={'sub': user.user_name}, expires_delta=refresh_token_expires
+    )
+    return {
+        'access_token': access_token,
+        'refresh_token': refresh_token,
+        'token_type': 'bearer'
+    }
+
+
+@router.post('/refresh_token', response_model=Token)
+async def refresh_token(form_data: RefreshToken):
+    user = await validate_token(token=form_data.refresh_token)
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={'sub': user.user_name, 'fresh': False}, expires_delta=access_token_expires
+    )
+    refresh_token_expires = timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+    refresh_token = create_refresh_token(
+        data={'sub': user.user_name}, expires_delta=refresh_token_expires
+    )
+    return {
+        'access_token': access_token,
+        'refresh_token': refresh_token,
+        'token_type': 'bearer'
+    }
