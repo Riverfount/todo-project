94ee8d31cf3995eb777ebf38b62987235698ab86
Vicente Marçal <vicente.marcal@gmail.com>
2023-10-25 21:29:09 -0300

    todo-project-68 Protegidas nossas rotas com login tanto de User quanto de SuperUser. (#71)

--- todo/routes/user.py
+++ todo/routes/user.py
@@ -4,18 +4,19 @@ from sqlmodel import Session
 
 from todo.db import ActiveSession
 from todo.models import User, UserDetailResponse, UserRequest, UserResponse, get_user
+from todo.auth import AuthenticatedUser, SuperUser
 
 router = APIRouter()
 
 
-@router.get('/', response_model=list[UserResponse])
+@router.get('/', response_model=list[UserResponse], dependencies=[AuthenticatedUser])
 async def list_users():
     """List all users."""
     users = get_user()
     return users
 
 
-@router.get('/{user_name}/', response_model=UserDetailResponse)
+@router.get('/{user_name}/', response_model=UserDetailResponse, dependencies=[AuthenticatedUser])
 async def get_user_by_user_name(*, user_name: str):
     """Get user by user_name"""
     user = get_user(user_name=user_name)
@@ -24,7 +25,7 @@ async def get_user_by_user_name(*, user_name: str):
     return user
 
 
-@router.post('/', response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+@router.post('/', response_model=UserResponse, status_code=status.HTTP_201_CREATED, dependencies=[SuperUser])
 async def create_user(*, user: UserRequest, session: Session = ActiveSession):
     """Creates new user"""
     # TODO: Validar informações recebidas com as constraints do banco.
